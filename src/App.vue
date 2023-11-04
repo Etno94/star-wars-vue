0a7ac829d936d92741b20ed3f352a1f63782293f
Etno94 <romero.juan.ignacio.1294@gmail.com>
2023-11-03 22:57:06 -0300

    initial component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MyComponent></MyComponent>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import MyComponent from './components/MyComponent.component.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MyComponent
   }
 }
 </script>
@@ -21,6 +21,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
 }
 </style>
---
+++ src/components/MyComponent.component.vue
@@ -0,0 +1,12 @@
+<template>
+    <img alt="Vue logo" src="./assets/QU-logo.png">
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'MyComponent'
+}
+</script>
